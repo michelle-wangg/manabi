208dedee935972b74783eecc071109e034731ce9
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-07 14:33:46 -0700

    init firebase

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Schedule from './components/Schedule';
 import ListOfMeetings from './components/ListOfMeetings';
 import "./App.css"
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// import 'firebase/auth';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
   apiKey: "AIzaSyDLDFidgQ5lLTVCmyMNIVg9Fb2VF6WiNjA",
   authDomain: "manabi-3117f.firebaseapp.com",
   projectId: "manabi-3117f",
@@ -18,10 +22,14 @@ firebase.initializeApp({
   messagingSenderId: "355917125009",
   appId: "1:355917125009:web:fa94c502c078571909f630",
   measurementId: "G-36CJQTE0L8"
-})
+};
 
-const auth = firebase.auth();
-const firestore = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
 
 // document.addEventListener("DOMContentLoaded", event => {
 //   const app = firebase.app(); 
