8240990a0ad9d7636a33f32b26c0463a25296e82
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-07 16:18:03 -0700

    landing page logic in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Schedule from './components/Schedule';
 import ListOfMeetings from './components/ListOfMeetings';
 import Landing from './components/Landing';
 import "./App.css"
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { auth } from './components/Firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from './components/Firebase';
 
 function App() {
 
-  // const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user])
 
   return (
-    <div style={{
+    user ? <div style={{
       backgroundColor: "#27374F",
       height: "100vh",
       minHeight: "100vh"
     }}>
       <h1 className="Title">Manabi.</h1>
-      <Schedule/>
-      <ListOfMeetings/>
-      <Landing />
-    </div>
-    
+      <Schedule />
+      <ListOfMeetings />
+    </div> : <Landing />
   )
 }
 
