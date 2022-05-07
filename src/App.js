2547424370212ac80729b8d9ee1da504d035dc7c
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-06 22:51:55 -0700

    Init react firebase hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import Schedule from './components/Schedule';
 import ListOfMeetings from './components/ListOfMeetings';
 import "./App.css"
 
+// document.addEventListener("DOMContentLoaded", event => {
+//   const app = firebase.app(); 
+//   console.log(app);
+// })
+
+// function googleLogin() {
+//   const provider = new firebase.auth.GoogleAuthProvider();
+
+//   firebase.auth().signInWithPopup(provider)
+
+//       .then(result => {
+//           const user = result.user;
+//           document.write('Hello ${user.displayName}');
+//       })
+// }
+
 function App() {
   return (
     <div style={{
