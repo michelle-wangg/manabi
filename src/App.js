0365275244c2f9da0601f8c2f535d123d90ffa23
michelle-wangg <michellewang124@gmail.com>
2022-05-06 20:34:36 -0700

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Schedule from './components/Schedule';
 import ListOfMeetings from './components/ListOfMeetings';
+import "./App.css"
 
 function App() {
   return (
-    <div className="schedule">
+    <div style={{
+      backgroundColor: "#27374F",
+      height: "100vh",
+      minHeight: "100vh"
+    }}>
+      <h1 className="Title">Manabi.</h1>
       <Schedule/>
       <ListOfMeetings/>
     </div>
