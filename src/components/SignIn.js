a661d5df0e471a59c001dbde5a4a90e1e6ebb2b2
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-07 16:01:17 -0700

    SignIn!

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-import 'firebase/auth';
+import { auth } from './Firebase.js';
 
 const SignIn = () => {
 
     const googleSignIn = () => {
         const provider = new GoogleAuthProvider();
-        signInWithPopup(provider)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error => {
-            console.log("bruh");
-        })
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+
+                const user = result.user;
+                console.log(user);
+            })
+            .catch(error => {
+                console.log("bruh");
+            })
     }
 
-return <button onClick={googleSignIn}>Google Sign In</button >
+    return <button onClick={googleSignIn}>Google Sign In</button >
 }
 
 export default SignIn
