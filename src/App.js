0337d81cccb1ffefe0a906af169af3c5fb10e2cb
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-05-07 13:21:51 -0700

    connected firebase to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import Schedule from './components/Schedule';
 import ListOfMeetings from './components/ListOfMeetings';
 import "./App.css"
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDLDFidgQ5lLTVCmyMNIVg9Fb2VF6WiNjA",
+  authDomain: "manabi-3117f.firebaseapp.com",
+  projectId: "manabi-3117f",
+  storageBucket: "manabi-3117f.appspot.com",
+  messagingSenderId: "355917125009",
+  appId: "1:355917125009:web:fa94c502c078571909f630",
+  measurementId: "G-36CJQTE0L8"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 // document.addEventListener("DOMContentLoaded", event => {
 //   const app = firebase.app(); 
 //   console.log(app);
