39708945c49e5984ef061f9ee4858300c98a1dfa
joanne <jlee2002@students.cs.ubc.ca>
2022-05-07 22:13:45 -0700

    Added popup panel

--- src/components/SetMeeting.js
+++ src/components/SetMeeting.js
@@ -5,10 +5,24 @@ const SetMeeting = props => {
     return ( 
         <div className='Popup'>
             <div className='InnerBox'>
-                heuofjroifjieojfiojoif
+                <div className='Horizontal'>
+                    <p className='InnerText'> Date </p>
+                    <p className='InnerText'> Time </p>
+                    <p className='InnerText'> Topic </p>
+                </div>
+                    <input type='text' className='InputBox'/>
+                    <input type='text' className='InputBox'/>
+                    <input type='text' className='InputBox'/>
+
+                <div className='Horizontal' style={{marginTop: 20}}>
+                    <p className='InnerText'> Link: </p>
+                    <input type='text' style={{marginLeft: 20}}/>
+                </div>
             </div>
 
-            <button className='ButtonAdd'>Add</button>
+            <button className='ButtonAdd'>
+                <p className='AddText'>Add</p>
+            </button>
             <button className='ButtonClose' 
                     style={{color: "white"}}
                     onClick = {props.handleClose}>x</button>
